85c624262ed95da0ccb9fa26dfbd1f385c400528
Saul M <saulmm2@gmail.com>
2015-03-05 19:51:27 +0100

    implemented Callbacks on the MainActivity

--- app/src/main/java/com/androidvigo/cloud/MainActivity.java
+++ app/src/main/java/com/androidvigo/cloud/MainActivity.java
@@ -4,20 +4,31 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 
+import java.util.List;
 
-public class MainActivity extends ActionBarActivity {
 
+public class MainActivity extends ActionBarActivity
+    implements GetMemesAsyncTask.GetMemesCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void startGetMemesRequest(View view) {
 
-        new GetMemesAsyncTask().execute();
+        new GetMemesAsyncTask(this).execute();
+    }
+
+    @Override
+    public void onMemesResult(List<MemeEntity> memesList) {
+
+    }
+
+    @Override
+    public void onMemesError() {
+
     }
 }
