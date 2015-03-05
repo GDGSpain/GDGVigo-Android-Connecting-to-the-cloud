dd5a3c1cf6530e24ca269dd63712fcde655e4263
Saul M <saulmm2@gmail.com>
2015-03-05 19:31:41 +0100

    implemented MEME's Json Parser

--- app/src/main/java/com/androidvigo/cloud/GetMemesAsyncTask.java
+++ app/src/main/java/com/androidvigo/cloud/GetMemesAsyncTask.java
@@ -4,6 +4,10 @@ package com.androidvigo.cloud;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,26 +15,57 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("UnusedDeclaration")
 public class GetMemesAsyncTask extends AsyncTask<Void, Void, String> {
 
     public static final String API_ENDPOINT = "http://alltheragefaces.com/api/all/faces";
 
+    private List<MemeEntity> mMemeEntityList;
+
+    @Override
+    protected void onPostExecute(String s) {
+
+        super.onPostExecute(s);
+    }
+
     @Override
     protected String doInBackground(Void... params) {
 
         // OMG Background thread
         String result = askWithHttpURLConnection();
-        Log.d("[DEBUG]", "GetMemesAsyncTask doInBackground - Result: "+result);
+
+        if (result != null) {
+
+            try {
+
+                JSONArray memeJSONArray = new JSONArray(result);
+                mMemeEntityList = new ArrayList<>(memeJSONArray.length());
+
+                for (int position = 0; position < memeJSONArray.length(); position++) {
+
+                    MemeEntity memeEntity = parseMemeJSON(memeJSONArray.getJSONObject(position));
+                    mMemeEntityList.add(memeEntity);
+                }
+
+                Log.d("[DEBUG]", "GetMemesAsyncTask doInBackground - Meme list size: "+mMemeEntityList.size());
+
+            } catch (JSONException e) {
+
+                Log.e("[ERROR]", "GetMemesAsyncTask, doInBackground (37)- " +
+                    "Error obtaining the meme array: "+e.getMessage());
+            }
+        }
 
         return null;
     }
 
-    public String askWithHttpURLConnection () {
+    public String askWithHttpURLConnection() {
 
         HttpURLConnection urlConnection = null;
-        BufferedReader reader           = null;
+        BufferedReader reader = null;
 
 
         String memeString = null;
@@ -82,7 +117,7 @@ public class GetMemesAsyncTask extends AsyncTask<Void, Void, String> {
         } catch (IOException e) {
 
             Log.e("[ERROR]", "MainActivity, askWithHttpURLConnection (45)- " +
-                "IOException: "+e.getMessage());
+                "IOException: " + e.getMessage());
 
         } finally {
 
@@ -95,7 +130,8 @@ public class GetMemesAsyncTask extends AsyncTask<Void, Void, String> {
 
                 try {
 
-                    reader.close();;
+                    reader.close();
+                    ;
 
                 } catch (final IOException e) {
 
@@ -106,6 +142,39 @@ public class GetMemesAsyncTask extends AsyncTask<Void, Void, String> {
         }
 
         return memeString;
+    }
+
+    public MemeEntity parseMemeJSON(JSONObject jsonObject) {
+
+        MemeEntity memeEntity = new MemeEntity();
+
+        try {
+
+            memeEntity.setTitle(jsonObject.getString("title"));
+            memeEntity.setEmotion(jsonObject.getString("emotion"));
+            memeEntity.setCanonical(jsonObject.getString("canonical"));
+            memeEntity.setCanonical(jsonObject.getString("created"));
+            memeEntity.setCategoryfk(jsonObject.getString("categoryfk"));
+            memeEntity.setTags(jsonObject.getString("tags"));
+            memeEntity.setViews(jsonObject.getString("views"));
+            memeEntity.setMoreinformation(jsonObject.getString("moreinformation"));
+            memeEntity.setIs_censored(jsonObject.getString("is_censored"));
+            memeEntity.setId(jsonObject.getString("id"));
+            memeEntity.setWeekly_views(jsonObject.getString("weekly_views"));
+            memeEntity.setPng(jsonObject.getString("png"));
+            memeEntity.setJpg(jsonObject.getString("jpg"));
+            memeEntity.setLargepng(jsonObject.getString("largepng"));
+            memeEntity.setSvg(jsonObject.getString("svg"));
+
+        } catch (JSONException e) {
+
+            memeEntity = null;
+
+            Log.e("[ERROR]", "GetMemesAsyncTask, parseMemeJSON (139)- " +
+                "Error: " + e.getMessage());
+        }
+
+        return memeEntity;
     }
 
 }
