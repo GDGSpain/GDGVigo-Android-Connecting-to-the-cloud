253ff7baec346be739194cb0d5f559239b044032
Saul M <saulmm2@gmail.com>
2015-03-05 19:15:54 +0100

    Created MemeEntity

---
+++ app/src/main/java/com/androidvigo/cloud/MemeEntity.java
@@ -0,0 +1,172 @@
+package com.androidvigo.cloud;
+
+
+@SuppressWarnings("UnusedDeclaration")
+public class MemeEntity {
+
+    private String title;
+    private String emotion;
+    private String canonical;
+    private String created;
+    private String categoryfk;
+    private String tags;
+    private String views;
+    private String moreinformation;
+    private String is_censored;
+    private String id;
+    private String weekly_views;
+    private String png;
+    private String jpg;
+    private String largepng;
+    private String svg;
+
+    public String getTitle() {
+
+        return title;
+    }
+
+    public void setTitle(String title) {
+
+        this.title = title;
+    }
+
+    public String getEmotion() {
+
+        return emotion;
+    }
+
+    public void setEmotion(String emotion) {
+
+        this.emotion = emotion;
+    }
+
+    public String getCanonical() {
+
+        return canonical;
+    }
+
+    public void setCanonical(String canonical) {
+
+        this.canonical = canonical;
+    }
+
+    public String getCreated() {
+
+        return created;
+    }
+
+    public void setCreated(String created) {
+
+        this.created = created;
+    }
+
+    public String getCategoryfk() {
+
+        return categoryfk;
+    }
+
+    public void setCategoryfk(String categoryfk) {
+
+        this.categoryfk = categoryfk;
+    }
+
+    public String getTags() {
+
+        return tags;
+    }
+
+    public void setTags(String tags) {
+
+        this.tags = tags;
+    }
+
+    public String getViews() {
+
+        return views;
+    }
+
+    public void setViews(String views) {
+
+        this.views = views;
+    }
+
+    public String getMoreinformation() {
+
+        return moreinformation;
+    }
+
+    public void setMoreinformation(String moreinformation) {
+
+        this.moreinformation = moreinformation;
+    }
+
+    public String getIs_censored() {
+
+        return is_censored;
+    }
+
+    public void setIs_censored(String is_censored) {
+
+        this.is_censored = is_censored;
+    }
+
+    public String getId() {
+
+        return id;
+    }
+
+    public void setId(String id) {
+
+        this.id = id;
+    }
+
+    public String getWeekly_views() {
+
+        return weekly_views;
+    }
+
+    public void setWeekly_views(String weekly_views) {
+
+        this.weekly_views = weekly_views;
+    }
+
+    public String getPng() {
+
+        return png;
+    }
+
+    public void setPng(String png) {
+
+        this.png = png;
+    }
+
+    public String getJpg() {
+
+        return jpg;
+    }
+
+    public void setJpg(String jpg) {
+
+        this.jpg = jpg;
+    }
+
+    public String getLargepng() {
+
+        return largepng;
+    }
+
+    public void setLargepng(String largepng) {
+
+        this.largepng = largepng;
+    }
+
+    public String getSvg() {
+
+        return svg;
+    }
+
+    public void setSvg(String svg) {
+
+        this.svg = svg;
+    }
+}
