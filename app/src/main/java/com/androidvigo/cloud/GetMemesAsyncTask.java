fb601a979ad127fb03bb6e4b667b4f32688c9269
Saul M <saulmm2@gmail.com>
2015-03-05 18:44:46 +0100

    Created GetMemesAsyncTask

---
+++ app/src/main/java/com/androidvigo/cloud/GetMemesAsyncTask.java
@@ -0,0 +1,32 @@
+package com.androidvigo.cloud;
+
+
+import android.os.AsyncTask;
+
+@SuppressWarnings("UnusedDeclaration")
+public class GetMemesAsyncTask extends AsyncTask<Void, Void, String> {
+
+    @Override
+    protected void onPreExecute() {
+
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+
+        super.onPostExecute(s);
+    }
+}
