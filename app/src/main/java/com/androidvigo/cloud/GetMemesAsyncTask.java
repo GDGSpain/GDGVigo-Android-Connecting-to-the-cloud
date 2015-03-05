d0f37eb16373e69e70f6bb0fd3d2b5ac96e54a61
Saul M <saulmm2@gmail.com>
2015-03-05 18:46:30 +0100

    added comments to show where the methods of the asynctask will be run

--- app/src/main/java/com/androidvigo/cloud/GetMemesAsyncTask.java
+++ app/src/main/java/com/androidvigo/cloud/GetMemesAsyncTask.java
@@ -9,24 +9,28 @@ public class GetMemesAsyncTask extends AsyncTask<Void, Void, String> {
     @Override
     protected void onPreExecute() {
 
+        // OMG Main thread
         super.onPreExecute();
     }
 
     @Override
     protected String doInBackground(Void... params) {
 
+        // OMG Background thread
         return null;
     }
 
     @Override
     protected void onProgressUpdate(Void... values) {
 
+        // OMG Main thread
         super.onProgressUpdate(values);
     }
 
     @Override
     protected void onPostExecute(String s) {
 
+        // OMG Main thread
         super.onPostExecute(s);
     }
 }
