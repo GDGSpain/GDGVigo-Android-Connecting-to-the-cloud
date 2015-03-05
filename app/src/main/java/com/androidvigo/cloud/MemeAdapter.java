e33b2c1855fc2dfd82c02d7185ae5ea867728136
Saul M <saulmm2@gmail.com>
2015-03-06 00:13:03 +0100

    Added Memes info into the custom adapter

--- app/src/main/java/com/androidvigo/cloud/MemeAdapter.java
+++ app/src/main/java/com/androidvigo/cloud/MemeAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -26,6 +27,15 @@ public class MemeAdapter extends ArrayAdapter<MemeEntity> {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         convertView = LayoutInflater.from(mContext).inflate(R.layout.item_meme, parent, false);
+
+        MemeEntity currentMeme = mMemeEntitiesList.get(position);
+
+        TextView titleTextView = (TextView) convertView.findViewById(R.id.item_meme_title);
+        TextView emotionTextView = (TextView) convertView.findViewById(R.id.item_meme_emotion);
+
+        titleTextView.setText(currentMeme.getTitle());
+        emotionTextView.setText(currentMeme.getEmotion());
+
         return convertView;
     }
 
